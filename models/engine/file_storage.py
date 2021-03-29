41db6cccdeafbf9741c935d0b95cc4c90e91f4af
Soledad Frechou <solefrechou@hotmail.com>
2021-03-29 15:51:11 +0000

    Fix pep8 file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,12 +8,13 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
- 
+
+
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
- 
+
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         if cls is not None:
@@ -25,11 +26,11 @@ class FileStorage:
             return filter_dict
         else:
             return FileStorage.__objects
- 
+
     def new(self, obj):
         """Adds new object to storage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
- 
+
     def save(self):
         """Saves storage dictionary to file"""
         with open(FileStorage.__file_path, 'w') as f:
@@ -38,7 +39,7 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
- 
+
     def reload(self):
         """Loads storage dictionary from file"""
         from models.base_model import BaseModel
@@ -48,7 +49,7 @@ class FileStorage:
         from models.city import City
         from models.amenity import Amenity
         from models.review import Review
-    
+
         classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
                     'State': State, 'City': City, 'Amenity': Amenity,
@@ -62,7 +63,7 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
- 
+
     def delete(self, obj=None):
         """Deletes obj from __objects"""
         dict_iter = FileStorage.__objects.copy()
