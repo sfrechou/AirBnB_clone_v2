aee937080e49819e24d7a90d4cffeb94ae39b2f1
Soledad Frechou <solefrechou@hotmail.com>
2021-04-15 21:57:52 +0000

    add

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -12,12 +12,12 @@ env.hosts = ['35.237.87.143', '35.237.66.227']
 def do_clean(number=0):
     """Deletes out-of-date archives"""
     if number == 0 or number == 1:
-        local("cd /versions/")
+        local("cd versions")
         local("ls -1t | tail -n 2 | xargs rm -f")
-        run("cd /data/web_static/releases/")
+        run("cd /data/web_static/releases")
         run("ls -1t | tail -n 2 | xargs rm -f")
     elif number >= 2:
-        local("cd /versions/")
+        local("cd versions")
         local("ls -1t | tail -n " + (number + 1) + " | xargs rm -f")
-        run("cd /data/web_static/releases/")
+        run("cd /data/web_static/releases")
         run("ls -1t | tail -n " + (number + 1) + " | xargs rm -f")
