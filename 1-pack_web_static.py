7155f7f64c855e70c56dd564c8bce8b7dc54d12d
Soledad Frechou <solefrechou@hotmail.com>
2021-04-15 04:40:43 +0000

    ADd

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,12 +5,13 @@ the contents of the web_static folder
 """
 from fabric.api import run
 from fabric.api import local
+from datetime import datetime
 
 
 def do_pack():
     """Generates a .tgz archive"""
     try:
-        run("mkdir -p versions")
+        local("mkdir -p versions")
         n = datetime.now()
         filename = "versions/web_static_" + n.strftime("%Y%m%d%H%M%S") + ".tgz"
         local("tar -cvzf " + filename + " web_static")
