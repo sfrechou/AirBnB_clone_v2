ba0a87e21b523a46bd17760df0814d6505a59bc2
Soledad Frechou <solefrechou@hotmail.com>
2021-04-16 00:18:55 +0000

    Add

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -10,30 +10,30 @@ exec {'mkdir':
                mkdir -p /data/web_static/releases/; mkdir -p /data/web_static/shared/;
                mkdir -p /data/web_static/releases/test/',
   provider => 'shell',
-  requided => Exec['install'],
+  required => Exec['install'],
 }
 
 exec {'echo':
   command  => 'echo "test_text" > /data/web_static/releases/test/index.html',
   provider => 'shell',
-  requided => Exec['mkdir'],
+  required => Exec['mkdir'],
 }
 
 exec {'ln':
   command  => 'ln -sf /data/web_static/releases/test/ /data/web_static/current',
   provider => 'shell',
-  requided => Exec['echo'],
+  required => Exec['echo'],
 }
 
 exec {'chown':
   command  => 'chown -R ubuntu:ubuntu /data/',
   provider => 'shell',
-  requided => Exec['ln'],
+  required => Exec['ln'],
 }
 
 exec {'sed':
   command  => 'sed -i "37i\ location /hbnb_static/ { \n\t alias /data/web_static/current/ ; \n }" /etc/nginx/sites-available/default;
                service nginx restart',
   provider => 'shell',
-  requided => Exec['chown'],
+  required => Exec['chown'],
 }
